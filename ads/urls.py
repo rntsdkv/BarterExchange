3fc2ab8bf32d399ea011072c526f2cbf9654cf91
Садыков Ренат Маратович <remasadykov@edu.hse.ru>
2025-04-12 12:13:25 +0300

    beautify urls

--- ads/urls.py
+++ ads/urls.py
@@ -7,13 +7,16 @@ from . import views
 urlpatterns = [
     #    path("admin/", admin.site.urls),
     path('', views.index, name='index'),
-    path('new_ad/', views.new_ad_form, name='new_ad'),
     path('auth/', LoginView.as_view(template_name='auth.html'), name='auth'),
     path('register/', views.register, name='register'),
+    path('new_ad/', views.new_ad_form, name='new_ad'),
     path('ad/<int:id>/', views.ad, name='ad'),
     path('ad/<int:id>/edit/', views.ad_edit, name='ad_edit'),
-    path('no_access/', views.no_access, name='no_access'),
     path('ad/<int:id>/delete/', views.ad_delete, name='ad_delete'),
+    path('ad/<int:id>/exchange/', views.ad_exсhange, name='ad_exсhange'),
+    path('exchange/<int:id>/', views.exchange, name='exchange'),
+    path('exchange/<int:id>/update/', views.exchange_update, name='exchange_update'),
+    path('no_access/', views.no_access, name='no_access'),
     path('search/', views.search, name='search'),
 ]
 
